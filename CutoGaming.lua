6cdd4875a1080d0920a60408f03b8c82b44d4212
Tromg09 <truongkhungtrongkhung@gmail.com>
2025-02-21 23:31:01 +0700

    Update CutoGaming.lua

--- CutoGaming.lua
+++ CutoGaming.lua
@@ -29,35 +29,7 @@ MinimizeButton({
       })
       
 ------ Tab
-     local Tab1o = MakeTab({Name = "W-Azure"})
-     
-        local Tab2o = MakeTab({Name = "AnDepZaiHubBeta"})
-
-local Tab3o = MakeTab({Name = "Main Fram"})
-------- BUTTON
-    
-    AddButton(Tab1o, {
-     Name = "Start",
-    Callback = function()
-	 getgenv().Team = "Marines"
-
-getgenv().FixCrash = false -- Turn it On For Hopping Server, Improve Performance But Silent Aim On Mob And Player
-
-getgenv().FixCrash2 = false -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
-
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
-  end
-  })
-  
-  AddButton(Tab2o, {
-     Name = "Start",
-    Callback = function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AnDepZaiHub/AnDepZaiHubBeta/refs/heads/main/AnDepZaiHubNewUpdated.lua"))()
-  end
-  })
-
-     
-     
+local Tab1o = MakeTab({Name = "Main Fram"})
 -------TOGGLE 
 
      Toggle = AddToggle(Tab1o, {
@@ -69,33 +41,33 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/AnDepZaiHub/AnDepZaiH
     
 ------- BUTTON
     
-    AddButton(Tab3o, {
+    AddButton(Tab1o, {
      Name = "FIX LAG",
     Callback = function()
 	  loadstring(game:HttpGet("https://raw.githubusercontent.com/TurboLite/Script/main/FixLag.lua"))()
   end
   })
-     AddButton(Tab3o, {
+     AddButton(Tab1o, {
      Name = "SCRIPT",
     Callback = function()
 	  loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/chillz-workshop/main/Arceus%20X%20V3"))()
   end
-  })   AddButton(Tab3o, {
+  })   AddButton(Tab1o, {
      Name = "2",
     Callback = function()
 	  loadstring(game:HttpGet("https://raw.githubusercontent.com/TurboLite/Script/main/FixLag.lua"))()
   end
-  })   AddButton(Tab3o, {
+  })   AddButton(Tab1o, {
      Name = "3",
     Callback = function()
 	  loadstring(game:HttpGet("https://raw.githubusercontent.com/TurboLite/Script/main/FixLag.lua"))()
   end
-  })   AddButton(Tab3o, {
+  })   AddButton(Tab1o, {
      Name = "4",
     Callback = function()
 	  loadstring(game:HttpGet("https://raw.githubusercontent.com/TurboLite/Script/main/FixLag.lua"))()
   end
-  })   AddButton(Tab3o, {
+  })   AddButton(Tab1o, {
      Name = "FIX LAG",
     Callback = function()
 	  loadstring(game:HttpGet("https://raw.githubusercontent.com/TurboLite/Script/main/FixLag.lua"))()
@@ -103,7 +75,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/AnDepZaiHub/AnDepZaiH
   })
 ----- Dropdown 
 
-  Dropdown = AddDropdown(Tab3o, {
+  Dropdown = AddDropdown(Tab1o, {
      Name = "a",
      Options = {"Test 1", "Test 2", "Test 3", "Test 4"},
      Default = "Melee",
